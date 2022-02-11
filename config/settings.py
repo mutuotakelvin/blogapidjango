9bc40016d723aa7b106b7569d990852af4995611
mutuotakelvin <kelvinmutuota749@gmail.com>
2022-02-11 12:20:31 +0300

    Add rest_framework to installed apps

--- config/settings.py
+++ config/settings.py
@@ -37,10 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd party
+    'rest_framework',
     # Local
     'posts',
-    
+
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES':[
+        'rest_framework.permissions.AllowAny'
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
