d4d2e15f8ebc907c0e1d9a34ea4b33e369901732
mutuotakelvin <kelvinmutuota749@gmail.com>
2022-02-11 12:46:21 +0300

    Refactor

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Post
 
-class PostSerializers(serializers.ModelSerializer):
+class PostSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('id','author','title','body','created_at',)
         model = Post
